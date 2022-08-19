98dbc63d9f2d8290721520e9ecc5b9b7b0abff44
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-19 18:26:44 +0300

    Update Sqr_solver.cpp
    добавил комменты

--- Sqr_solver.cpp
+++ Sqr_solver.cpp
@@ -6,7 +6,8 @@ int main()
 {
     float a, b, c;
     double D;
-
+    
+    /* Ввод a, b, c */
     printf("Введите коэфф. а\n");
     if (!(scanf("%f", &a))) {
         printf("Введён неправильный формат данных\n");
@@ -22,12 +23,13 @@ int main()
         printf("Введён неправильный формат данных\n");
         return 1;
     }
-
+    
+    /* случаи */
     if (a >= 0 && a <= 0 && b >= 0 && b <= 0 && c >= 0 && c <= 0) {
         printf("Корнем ур-я является любое число");
         return 0;
     }
-    else if (a >= 0 && a <= 0 && (b > 0 || b < 0)) {
+    else if (a >= 0 && a <= 0 && (b > 0 || b < 0)) { /* линейный случай */
         printf("Корень ур-я = %.5f\n", -c/b);
         return 0;
     }
@@ -36,7 +38,7 @@ int main()
         return 0;
     }
 
-    D = (double)pow(b, 2) - 4*a*c;
+    D = (double)pow(b, 2) - 4*a*c; /* дискриминант */
     if (D > 0)
         printf("x1 = %.4f, x2 = %.4f\n", ((-b + (double)sqrt(D))/(2*a)), ((-b - (double)sqrt(D))/(2*a)));
     else if (D >= 0 && D <= 0)
