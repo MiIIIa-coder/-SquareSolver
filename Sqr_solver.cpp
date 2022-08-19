682fc51e29d203ed2c49b2fc482a429d535900b7
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-19 17:48:11 +0300

    first
    8 ошибок

---
+++ Sqr_solver.cpp
@@ -0,0 +1,49 @@
+#include <TXLib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c;
+    double D;
+
+    printf("Введите коэфф. а\n");
+    if (!(scanf("%f", &a))) {
+        printf("Введён неправильный формат данных\n");
+        return 1;
+    }
+    printf("Введите коэфф. b\n");
+    if (!(scanf("%f", &b))) {
+        printf("Введён неправильный формат данных\n");
+        return 1;
+    }
+    printf("Введите коэфф. c\n");
+    if (!(scanf("%f", &c))) {
+        printf("Введён неправильный формат данных\n");
+        return 1;
+    }
+
+    if (a == 0 && b == 0 && c == 0) {
+        printf("Корнем ур-я является любое число");
+        return 0;
+    }
+    else if (a == 0 && b != 0) {
+        printf("Корень ур-я = %.5f\n", -c/b);
+        return 0;
+    }
+    else if (a == 0 && b == 0) {
+        printf("Корней нет\n");
+        return 0;
+    }
+
+    D = (double)pow(b, 2) - 4*a*c;
+    if (D > 0)
+        printf("x1 = %.4f, x2 = %.4f\n", ((-b + (double)sqrt(D))/(2*a)), ((-b - (double)sqrt(D))/(2*a)));
+    else if (D == 0)
+        printf("x1 = x2 = %.4f\n", (-b/(2*a)));
+    else
+        printf("Корней нет\n");
+
+    return 0;
+}
+
