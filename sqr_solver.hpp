0e436dbd7ca92c22c8c552c02d14371885537143
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-23 17:24:51 +0300

    Update sqr_solver.hpp

--- sqr_solver.hpp
+++ sqr_solver.hpp
@@ -5,14 +5,14 @@
 #include <stdio.h>
 #include <math.h>
 
-bool comparing(double comp_digit);  /* сравнение с 0, True <=> равен */
-void input(double *a, double *b, double *c, int *count_ans);  /* ввод коэффициентов */
-void line(double b, double c, double *x1, int *count_ans); /* если не квадратное */
+bool comparing(double comp_digit);  /* СЃСЂР°РІРЅРµРЅРёРµ СЃ 0, True <=> СЂР°РІРµРЅ */
+void input(double *a, double *b, double *c, int *count_ans);  /* РІРІРѕРґ РєРѕСЌС„С„РёС†РёРµРЅС‚РѕРІ */
+void line(double b, double c, double *x1, int *count_ans); /* РµСЃР»Рё РЅРµ РєРІР°РґСЂР°С‚РЅРѕРµ */
 void b_null(double a, double c, double *x1, double *x2, int *count_ans); // b == 0
 void c_null(double a, double b, double *x1, double *x2, int *count_ans); // c == 0
-void sqr_solve(double a, double b, double c, double *x1, double *x2, int *count_ans); /* решение квадратного */
-void output(int *count_ans, double *x1, double *x2); /* вывод ответа */
+void sqr_solve(double a, double b, double c, double *x1, double *x2, int *count_ans); /* СЂРµС€РµРЅРёРµ РєРІР°РґСЂР°С‚РЅРѕРіРѕ */
+void output(int *count_ans, double *x1, double *x2); /* РІС‹РІРѕРґ РѕС‚РІРµС‚Р° */
 
-enum amount {ZERO, ONE, TWO, INFINITY_}; //кол-во корней
+enum amount {ZERO, ONE, TWO, INFINITY_}; //РєРѕР»-РІРѕ РєРѕСЂРЅРµР№
 
 #endif // SQR_SOLVER_H_INCLUDED
