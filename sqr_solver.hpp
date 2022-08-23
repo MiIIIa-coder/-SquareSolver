77762173eab4c7f088f1a275f370dccf15a567aa
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-23 19:45:37 +0300

    Update sqr_solver.hpp

--- sqr_solver.hpp
+++ sqr_solver.hpp
@@ -5,14 +5,15 @@
 #include <stdio.h>
 #include <math.h>
 
-bool comparing(double comp_digit);  /* сравнение с 0, True <=> равен */
-void input(double *a, double *b, double *c, int *count_ans);  /* ввод коэффициентов */
-void line(double b, double c, double *x1, int *count_ans); /* если не квадратное */
-void b_null(double a, double c, double *x1, double *x2, int *count_ans); // b == 0
-void c_null(double a, double b, double *x1, double *x2, int *count_ans); // c == 0
-void sqr_solve(double a, double b, double c, double *x1, double *x2, int *count_ans); /* решение квадратного */
-void output(int *count_ans, double *x1, double *x2); /* вывод ответа */
+enum AMOUNT_ROOT {ZERO_ROOT, ONE_ROOT, TWO_ROOT, INFINITY_ROOT};             // кол-во корней
 
-enum amount {ZERO, ONE, TWO, INFINITY_}; //кол-во корней
+bool is_null(double comp_digit);                                             // сравнение с 0, True <=> равен
+void input(double *a, double *b, double *c, int count_ans);                  // ввод коэффициентов
+AMOUNT_ROOT line_equation(double b, double c, double *x1);                   // если не квадратное
+AMOUNT_ROOT b_null(double a, double c, double *x1, double *x2);              // b == 0
+AMOUNT_ROOT c_null(double a, double b, double *x1, double *x2);              // c == 0
+AMOUNT_ROOT sqr_solve(double a, double b, double c, double *x1, double *x2); // решение квадратного
+void solver(int a, int b, int c, int *count_ans, double *x1, double *x2);    // вызывает функцию решения для данного случая
+void output(int count_ans, double *x1, double *x2);                          // вывод ответа
 
 #endif // SQR_SOLVER_H_INCLUDED
