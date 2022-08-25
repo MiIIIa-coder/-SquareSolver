b660393a46d017bef1069921358fc6f2aee7cf53
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-25 20:16:37 +0300

    Add files via upload

---
+++ choice_selection.hpp
@@ -0,0 +1,14 @@
+/*!
+  @file choice_selection.hpp
+  @brief Functions for test of program.
+*/
+
+#ifndef CHOICE_SELECTION_HPP_INCLUDED
+#define CHOICE_SELECTION_HPP_INCLUDED
+
+/*!
+  @brief function to select the operating mode and calling functions that solve an equation .
+*/
+int start_function(void);
+
+#endif // CHOICE_SELECTION_HPP_INCLUDED
---
+++ test_SqrEq.hpp
@@ -0,0 +1,30 @@
+/*!
+  @file test_SqrEq.hpp
+  @brief Functions for test of program.
+*/
+
+#ifndef TEST_SQREQ_HPP_INCLUDED
+#define TEST_SQREQ_HPP_INCLUDED
+
+/*!
+  @brief test - function.
+*/
+int test_SqrEq(void);
+
+/*!
+  @brief function tests received roots.
+
+     @param [in]  a           a - coefficient x^2
+     @param [in]  b           b - coefficient x^1
+     @param [in]  c           c - coefficient x^0
+     @param [in]  x1          x1 - pointer to the first root
+     @param [in]  x2          x2 - pointer to the second root
+     @param [in]  count_ans   count_ans - count of roots
+     @param [in]  right_count right_count - number of roots from file
+     @param [in]  root1       root1 - first root from file
+     @param [in]  root2       root2 - second root from file
+*/
+void test_compare(double a, double b, double c, int count_ans, double x1, double x2, int right_count, double root1, double root2);
+
+
+#endif // TEST_SQREQ_HPP_INCLUDED
