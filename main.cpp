3766ba70fd73d698f219d239793c1be146d9232f
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-25 20:07:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
+/*!
+  @file main.cpp
+  @brief main file!
+*/
+
+#include "choice_selection.hpp"
 #include "sqr_solver.hpp"
+
 
 int main()
 {
-    double x1 = 0;
-    double x2 = 0;
+    
 
-    double a = 0;
-    double b = 0;
-    double c = 0;
-
-    int count_ans = -1;
-
-    input(&a, &b, &c, count_ans);
-
-    solver(a, b, c, &count_ans, &x1, &x2);
-
-    output(count_ans, &x1, &x2);
+    if (start_function() == 1)
+        return 1;
 
     return 0;
 }
