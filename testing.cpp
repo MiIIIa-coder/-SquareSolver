8311c1728a8c1e6ead902c564a7ec273a8e16d0c
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-27 19:45:13 +0300

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -6,6 +6,11 @@
 #include "sqr_solver.hpp"
 #include "compare.hpp"
 #include "test_SqrEq.hpp"
+#include "io.hpp"
+
+#define MAX_LEN 100
+
+static char warning_error[57] = "ERROR: an error occurred while trying to get file data.";
 
 int test_SqrEq(void)
 {
@@ -16,23 +21,39 @@ int test_SqrEq(void)
     double b = 0;
     double c = 0;
 
+    int check_fscanf = 0;
     int right_count = 0;
     int test_num = 0;
+
     double root1 = 0;
     double root2 = 0;
 
+    char name_test_file[MAX_LEN];
+
     int count_ans = -1;
 
+    get_name_test_file(name_test_file, MAX_LEN);
+
     FILE *test_file;
-    if ((test_file = fopen("Test_digits.txt", "r")) == NULL) {
-        printf("ERROR: failed to open file \"Test_digits.txt\"\n");
+    if ((test_file = fopen(name_test_file, "r")) == NULL) {
+        printf("ERROR: failed to open file %s\n", name_test_file);
         return 1;
     }
 
     printf("Function \"solver\" test starts now!\n");
 
 
-    while (fscanf(test_file, "%lg %lg %lg %d", &a, &b, &c, &right_count) != EOF) {
+    do {
+
+        check_fscanf = fscanf(test_file, "%lg %lg %lg %d", &a, &b, &c, &right_count);
+
+        if (check_fscanf == EOF) {
+            return 0;
+        }
+        else if (check_fscanf != 4) {
+            printf("%s\n", warning_error);
+            return 1;
+        }
 
         switch(right_count)
         {
@@ -41,14 +62,14 @@ int test_SqrEq(void)
 
             case TWO_ROOT:
                 if (fscanf(test_file, "%lg %lg", &root1, &root2) == EOF) {
-                    printf("ERROR: an error occurred while trying to get file data.");
+                    printf("%s\n", warning_error);
                     return 1;
                 }
                 break;
 
             case ONE_ROOT:
                 if (fscanf(test_file, "%lg", &root1) == EOF) {
-                    printf("ERROR: an error occurred while trying to get file data.");
+                    printf("%s\n", warning_error);
                     return 1;
                 }
                 break;
@@ -66,23 +87,20 @@ int test_SqrEq(void)
 
         test_compare(a, b, c, count_ans, x1, x2, right_count, root1, root2);
 
-    }
+    } while (check_fscanf == 4);
 
     if (fclose(test_file) == EOF) {
-        printf("ERROR: failed to close file \"Test_digits.txt\"\n");
+        printf("ERROR: failed to close file %s\n", name_test_file);
         return 1;
     }
-
-    // include                   // *
-    // errno
-    // perror
 
     return 0;
 }
 
 
 
-void test_compare(double a, double b, double c, int count_ans, double x1, double x2, int right_count, double root1, double root2)
+void test_compare(double a, double b, double c, int count_ans, double x1, double x2,
+                  int right_count, double root1, double root2)
 {
 
     if(!(count_ans == right_count && is_equal(x1, root1) && is_equal(x2, root2))) {
@@ -93,56 +111,6 @@ void test_compare(double a, double b, double c, int count_ans, double x1, double
     }
     else
         printf("SUCCESS!\n");
-
-/*
-    switch(right_count)
-    {
-        case INFINITY_ROOT:
-            if (count_ans == INFINITY_ROOT)
-                printf("The equation is correct!\n");
-                printf("FAILED:   count_ans = %d, x1 = %lg, x2 = %lg\n"
-                       "EXPECTED: count_ans = %d, x1 = %lg, x2 = %lg\n",
-                        count_ans, x1, x2, right_count, root1, root2);
-            else {
-                printf("Received coefficients: a = %lg b = %lg c = %lg\n", a, b, c);
-            }
-            break;
-
-        case TWO_ROOT:
-            if(!(count_ans == right_count && is_equal(x1, root1) && is_equal(x2, root2))) {
-                printf("Received coefficients: a = %lg b = %lg c = %lg\n", a, b, c);
-                printf("FAILED:   count_ans = %d, x1 = %lg, x2 = %lg\n"
-                       "EXPECTED: count_ans = %d, x1 = %lg, x2 = %lg\n",
-                        count_ans, x1, x2, right_count, root1, root2);
-            }
-            else
-                printf("The equation is correct!\n");
-            break;
-
-        case ONE_ROOT:
-            if (!(count_ans == right_count && is_equal(x1, root1))) {
-                printf("Received coefficients: a = %lg b = %lg c = %lg\n", a, b, c);
-                printf("FAILED:   count_ans = %d, x1 = %lg\n"
-                       "EXPECTED: count_ans = %d, x1 = %lg\n",
-                        count_ans, x1, right_count, root1);
-            }
-            else
-                printf("The equation is correct!\n");
-
-        case ZERO_ROOT:
-            if (count_ans == ZERO_ROOT)
-                printf("The equation is correct!\n");
-            else {
-                printf("FAILED:   count_ans = %d, x1 = %lg\n"
-                       "EXPECTED: count_ans = %d, x1 = %lg\n",
-                        count_ans, x1, right_count, root1);
-            }
-            break;
-
-        default:
-            printf("Incorrect amount of roots in testing\n");
-            break;
-    }
-    */
+  
     printf("\n");
 }
