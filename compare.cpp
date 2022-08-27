970c57124c95819986bf2fef2ea7046e03ffe8ce
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-27 19:44:22 +0300

    Update compare.cpp

--- compare.cpp
+++ compare.cpp
@@ -6,18 +6,14 @@
 #include "sqr_solver.hpp"
 #include "compare.hpp"
 
-static const double eps = 0.00001;
+static const double EPS = 0.00001;
 
 bool is_null(double value)
 {
-    if (fabs(value - 0.0) <= eps)
-        return true;
-    return false;
+    return (fabs(value - 0.0) <= EPS);
 }
 
 bool is_equal(double n1, double n2)
 {
-    if (fabs(n1 - n2) <= eps)
-        return true;
-    return false;
+    return (fabs(n1 - n2) <= EPS);
 }
