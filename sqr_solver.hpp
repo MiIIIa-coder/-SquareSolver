59e0777c35523d9d213c52726b1e8c6305e6b16d
MiIIIa-coder <111503013+MiIIIa-coder@users.noreply.github.com>
2022-08-30 12:36:40 +0300

    Update sqr_solver.hpp

--- sqr_solver.hpp
+++ sqr_solver.hpp
@@ -6,8 +6,9 @@
 #ifndef SQR_SOLVER_H_INCLUDED
 #define SQR_SOLVER_H_INCLUDED
 
-#include <TXLib.h>
+// #include <TXLib.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <math.h>
 
 /*!
@@ -22,11 +23,11 @@
   @brief lists constants numerically equal to the possible number of roots of the equation (here infinity = 3)
 */
 enum Amount_Root {
-    ZERO_ROOT,
-    ONE_ROOT,
-    TWO_ROOT,
-    INFINITY_ROOT
-    };
+    ZERO_ROOT     = 0,
+    ONE_ROOT      = 1,
+    TWO_ROOT      = 2,
+    INFINITY_ROOT = 3
+};
 
 /*!
   @brief function solves the equation, if it has linear kind or kind is 'const = 0' or kind of the equation is 0 = 0.
@@ -35,7 +36,7 @@ enum Amount_Root {
      @param [in] c    c - coefficient x^0
      @param [in, out] x1 - pointer to the first root
 */
-Amount_Root line_equation(double b, double c, double *x1);
+Amount_Root solve_linear_equation(double b, double c, double *x1);
 
 /*!
   @brief function solves equation, if coefficients: b = 0, a != 0.
@@ -45,7 +46,7 @@ Amount_Root line_equation(double b, double c, double *x1);
      @param [in, out]         x1 - pointer to the first root
      @param [in, out]         x2 - pointer to the second root
 */
-Amount_Root b_null(double a, double c, double *x1, double *x2);
+Amount_Root solve_b_null(double a, double c, double *x1, double *x2);
 
 /*!
   @brief function solves equation, if coefficients: c = 0, a != 0.
@@ -55,7 +56,7 @@ Amount_Root b_null(double a, double c, double *x1, double *x2);
      @param [in, out]         x1 - pointer to the first root
      @param [in, out]         x2 - pointer to the second root
 */
-Amount_Root c_null(double a, double b, double *x1, double *x2);
+Amount_Root solve_c_null(double a, double b, double *x1, double *x2);
 
 /*!
   @brief function solves equation, if its kind is square.
@@ -66,7 +67,7 @@ Amount_Root c_null(double a, double b, double *x1, double *x2);
      @param [in, out]         x1 - pointer to the first root
      @param [in, out]         x2 - pointer to the second root
 */
-Amount_Root sqr_solve(double a, double b, double c, double *x1, double *x2);
+Amount_Root solve_sqr(double a, double b, double c, double *x1, double *x2);
 
 /*!
   @brief function asks to other functions (line_equation, b_null, c_null, sqr_solve) to solve equation.
@@ -78,6 +79,6 @@ Amount_Root sqr_solve(double a, double b, double c, double *x1, double *x2);
      @param [in, out]  x2          x2 - pointer to the second root
      @param [in, out]  count_ans   count_ans - count of roots
 */
-void solver(double a, double b, double c, int *count_ans, double *x1, double *x2);
+void solve(double a, double b, double c, int *n_roots, double *x1, double *x2);
 
 #endif // SQR_SOLVER_H_INCLUDED
